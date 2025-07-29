faf88e656bd6d9e313bf078c6a40aaddc0649ba9
Sylvain Lesage <severo@rednegra.net>
2025-07-29 18:17:47 -0400

    upgrade to hightable 0.8.1 and convert the Dataframe (#28)

--- hightable/src/data.tsx
+++ hightable/src/data.tsx
@@ -1,4 +1,10 @@
-import { AsyncRow, rowCache, sortableDataFrame, wrapPromise, wrapResolved } from 'hightable'
+import { DataFrameEvents, ResolvedValue, UnsortableDataFrame, createEventTarget, sortableDataFrame } from 'hightable'
+
+export function checkSignal(signal?: AbortSignal): void {
+  if (signal?.aborted) {
+    throw new DOMException('The operation was aborted.', 'AbortError')
+  }
+}
 
 function lorem(rand: number, length: number): string {
   const words = 'lorem ipsum dolor sit amet consectetur adipiscing elit'.split(' ')
@@ -6,39 +12,88 @@ function lorem(rand: number, length: number): string {
   return str[0].toUpperCase() + str.slice(1)
 }
 
-function delay<T>(value: T, ms: number): Promise<T> {
-  return new Promise(resolve => setTimeout(() => { resolve(value) }, ms))
+function delayResolve<T>({ value, ms, signal }: {value: T, ms?: number, signal?: AbortSignal}): Promise<ResolvedValue<T>> {
+  return new Promise(resolve => setTimeout(
+    () => {
+      checkSignal(signal)
+      resolve({ value })
+    },
+    ms ?? 100 * Math.floor(10 * Math.random()),
+  ))
 }
 
+const numRows = 10_000
 const header = ['ID', 'Name', 'Age', 'UUID', 'Text', 'JSON']
-const mockData = {
+const eventTarget = createEventTarget<DataFrameEvents>()
+const cellCache = new Map<string, ResolvedValue[]>(header.map(column => [column, []]))
+const rowNumberCache : ResolvedValue<number>[] = []
+
+function generateValue({ row, column }: { row: number, column: string }): string | number {
+  switch (column) {
+  case 'ID':
+    return row + 1
+  case 'Name':
+    return `Name${row}`
+  case 'Age':
+    return 20 + row % 80
+  case 'UUID':
+    return crypto.randomUUID()
+  case 'Text':
+    return lorem( Math.abs(Math.sin(row + 1)), 10)
+  case 'JSON':
+    return JSON.stringify({ row, column })
+  default:
+    throw new Error(`Unknown column: ${column}`)
+  }}
+
+const mockData: UnsortableDataFrame = {
   header,
-  numRows: 10000,
-  rows({ start, end }: { start: number, end: number }) {
-    const arr: AsyncRow[] = []
-    for (let i = start; i < end; i++) {
-      const rand = Math.abs(Math.sin(i + 1))
-      const uuid = rand.toString(16).substring(2)
-      const partial = {
-        ID: i + 1,
-        Name: `Name${i}`,
-        Age: 20 + i % 80,
-        UUID: uuid,
-        Text: lorem(rand, 100),
-      }
-      const row = { ...partial, JSON: JSON.stringify(partial) }
-      // Map to randomly delayed promises
-      const cells = Object.fromEntries(Object.entries(row).map(([key, value]) =>
-        // discrete time delay for each cell to simulate async data loading
-        [key, wrapPromise(delay(value, 100 * Math.floor(10 * Math.random())))],
-      ))
-      arr.push({
-        index: wrapResolved(i),
-        cells,
-      })
-    }
-    return arr
+  numRows,
+  getCell: ({ row, column }) => {
+    return cellCache.get(column)?.[row]
   },
+  getRowNumber: ({ row }) => {
+    return rowNumberCache[row]
+  },
+  fetch: async ({ rowEnd, rowStart, columns, signal }) => {
+    checkSignal(signal)
+    const promises: Promise<void>[] = []
+    for (let row = rowStart; row < rowEnd; row++) {
+      // fetch row number
+      if (!rowNumberCache[row]) {
+        promises.push(
+          delayResolve({ value: row, signal })
+            .then(resolved => {
+              rowNumberCache[row] = resolved
+              eventTarget.dispatchEvent(new CustomEvent('resolve'))
+            }),
+        )
+      }
+
+      // fetch cells
+      for (const column of columns ?? []) {
+        if (!header.includes(column)) {
+          throw new Error(`Unknown column: ${column}`)
+        }
+        if (!cellCache.get(column)?.[row]) {
+          promises.push(
+            delayResolve({ value: generateValue({ row, column }), signal })
+              .then(resolved => {
+                const columnCache = cellCache.get(column)
+                if (!columnCache) {
+                  throw new Error(`Column cache not found for: ${column}`)
+                }
+                columnCache[row] = resolved
+                eventTarget.dispatchEvent(new CustomEvent('resolve'))
+              }),
+          )
+        }
+      }
+    }
+
+    await Promise.all(promises)
+  },
+  eventTarget,
 }
 
-export const data = rowCache(sortableDataFrame(mockData))
+export const data = sortableDataFrame(mockData)
